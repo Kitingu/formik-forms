8a5fd42857f7fd0386d1a53f103b802a31e2ded7
kitingu <benlegendj@gmail.com>
2020-01-27 06:41:31 -0500

    formik radio and checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,67 @@
 import React from "react";
-import { Formik,Field } from "formik";
-import { TextField, Button } from "@material-ui/core";
+import { Formik, Field, Form } from "formik";
+import { TextField, Button, Checkbox,Radio } from "@material-ui/core";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div>
       <Formik
-        initialValues={{ firstName: "ben", lastName: "mwendwa" }}
-        onSubmit={(data, { setSubmitting /*resetForm */ }) => {
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          isTall: false,
+          cookies: [],
+          yoghurt: ""
+        }}
+        onSubmit={async(data, { setSubmitting /*resetForm */ }) => {
           console.log(data);
           setSubmitting(false);
           // resetForm(false)
         }}
       >
-        {({ values, isSubmitting, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
+        {({ values, isSubmitting }) => (
+          <Form>
+            <Field
+              placeholder="First Name"
+              name="firstName"
+              type="input"
+              as={TextField}
+            />
+            <div>
+              <Field
+                placeholder="Last Name"
+                name="lastName"
+                type="input"
+                as={TextField}
+              />
+            </div>
 
-            
-            <TextField
-              name="firstname"
-              value={values.firstName}
-              onChange={handleChange}
-              onBlur={handleBlur}
+            <Field name="isTall" type="checkbox" as={Checkbox} />
+            <div> Cookies</div>
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="weed chip"
+              as={Checkbox}
             />
 
-            <TextField
-              name="lastname"
-              value={values.lastName}
-              onChange={handleChange}
-              onBlur={handleBlur}
+            <Field name="cookies" type="checkbox" value="sugar" as={Checkbox} />
+
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="choclate chip"
+              as={Checkbox}
             />
+            <div>Yoghurt</div>
+            <Field
+              name="yoghurt"
+              type="radio"
+              value="Strawbery"
+              as={Radio}
+            />
+            <Field name="yoghurt" type="radio" value="Vanilla " as={Radio} />
             <div>
               <Button disabled={isSubmitting} type="submit">
                 Submit
@@ -38,7 +69,7 @@ const App: React.FC = () => {
             </div>
 
             <pre> {JSON.stringify(values, null, 2)} </pre>
-          </form>
+          </Form>
         )}
       </Formik>
     </div>
